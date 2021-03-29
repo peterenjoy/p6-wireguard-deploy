9af177381215068952a2f324f0adf509369447a8
Pierre H <p.hellequin@gmail.com>
2021-03-29 18:33:00 +0200

    2nd test

---
+++ script_p6.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+
+# importe le csv des noms, prenom et email
+import pandas as pan
+db = pan.read_csv("/Users/pierrehellequin/p6/p6.csv")
+
+
+# variable dataset employ
+
+# vérifie si csv complet? 
+
+/pi# compte le nom de ligne
+
+# create 
+
+# Hello World First 
+a = 15
+print ("Hello World!" , a );
+
+# Importer le csv test
+
+# Ajouter un id 
